ed839ef8d1437c05a84c9c11fd33c2c331e8255a
Penuel Maypa <x16382003@student.ncirl.ie>
2017-02-03 10:20:14 +0000

    My Commit

---
+++ Programming/Document4.java
@@ -0,0 +1,41 @@
+/*
+*Mystery.java
+*@author Frances Sheridan
+*21/08/2012
+*/
+
+import java.util.Scanner;
+class Document4{
+
+public static void main(String args[]){
+
+//declare object(s)
+Scanner keyboard;
+
+//create object(s)
+keyboard = new Scanner(System.in);
+
+//declare contstant(s)
+final double X = 20.5;
+final int NUM = 10;
+
+//declare variable(s)
+int a, b;
+double z;
+
+//Get Input
+System.out.println("Enter the first integer:");
+a = keyboard.nextInt();
+
+System.out.println("Enter the second integer:");
+b = keyboard.nextInt();
+
+//process
+z = X + 2 * a - b;
+
+//output
+System.out.println("z = "+z);
+
+}
+
+}
---
+++ Programming/Lab Work (Fri 3-Feb-17)/Exercise 3/DebugTwo3.java
@@ -0,0 +1,20 @@
+public class DebugTwo3
+// Demonstrates remainder and output
+{
+  public static void main(String args[])
+  {
+      double a , b, result;
+      long c ;
+
+      c = 7777777777777L;
+      a = 99 ;
+      b = 8 ;
+      result = a % b;
+
+      System.out.println("Divide " + a + " by " + b);
+      System.out.println("remainder is " + result);
+      System.out.println("c is a very large number:");
+      System.out.println(c);
+   }
+
+}
